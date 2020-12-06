9a267ef5766096eabf3f5a50c791bb14be57a762
baraking <barakatz.bk@gmail.com>
2020-12-06 20:21:49 +0200

    added players

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public static readonly string PLAYER_TAG_NAME="player";
+    public static readonly string PLAYER_GENERIC_GAMEOBJECT_NAME = "Player";
+
+    public int playerIndex;
+    public Color playerColor;
+
+    public bool isPlayerActivated;
+
+    public Image cursor;
+
+    private void Awake()
+    {
+        cursor.color = playerColor;
+    }
+
+    public void UpdatePlayerColor()
+    {
+        cursor.color = playerColor;
+    }
+
+}
